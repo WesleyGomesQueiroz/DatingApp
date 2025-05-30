8ce1fac79f94a979a91b114ac7f2ab1b79c5a30a
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2025-05-30 10:24:24 -0300

    Adding the create token logic

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,13 +1,39 @@
 ﻿using API.Entities;
 using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API.Services
 {
-    public class TokenService : ITokenService
+    public class TokenService(IConfiguration config) : ITokenService
     {
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var tokenKay = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from appsettings");
+            if(tokenKay.Length < 64) throw new Exception("Your tokenKey needs to be longer");
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKay));
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserName)
+            };
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
