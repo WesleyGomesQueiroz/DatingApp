a7fe55c0758c04471ea626c11e0c92268dd69abe
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2025-07-27 20:31:10 -0300

    Creating a repository

---
+++ API/Interfaces/IUserController.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Udate(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<AppUser?> GetUserByIdAsync(int id);
+        Task<AppUser?> GetUserByUsernameAsync(string username);
+    }
+}
