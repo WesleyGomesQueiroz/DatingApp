beec5a2d5ff109ad2bdecf5e244b4022f9d60138
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2025-07-27 20:59:33 -0300

    Updating the users controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -7,22 +8,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Authorize]
-    public class UsersController(DataContext context) : BaseApiController
+    //[Authorize]
+    public class UsersController(IUserRepository userRepository) : BaseApiController
     {
-        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = await context.Users.ToListAsync();
-            return users;
+            var users = await userRepository.GetUsersAsync();
+            return Ok(users);
         }
 
-        [Authorize]
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        [HttpGet("{username}")]
+        public async Task<ActionResult<AppUser>> GetUsers(string username)
         {
-            var user = await context.Users.FindAsync(id);
+            var user = await userRepository.GetUserByUsernameAsync(username);
 
             if (user == null) return NotFound();
 
