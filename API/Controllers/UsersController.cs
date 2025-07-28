8d4ab59d3ca585c7049729e778cf6748707e2f48
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2025-07-27 21:31:35 -0300

    Adding a DTO for Members

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class UsersController(IUserRepository userRepository) : BaseApiController
     {
         [HttpGet]
