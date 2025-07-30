86056b9651ff4b79c2af47315c409920590d887f
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2025-07-30 19:47:59 -0300

    Adding a member service

--- client/src/app/errors/test-erros/test-erros.component.ts
+++ client/src/app/errors/test-erros/test-erros.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-test-erros',
@@ -9,7 +10,7 @@ import { Component, inject } from '@angular/core';
   styleUrl: './test-erros.component.css'
 })
 export class TestErrosComponent {
-  baseUrl = 'https://localhost:7227/api/';
+  baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
   validationErrors: string[] = [];
 
