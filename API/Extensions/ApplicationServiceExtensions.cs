328b4b4eb7a79047fa19f151844fd78335de0dca
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2025-08-04 21:35:12 -0300

    Adding a photo service

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,6 +18,7 @@ namespace API.Extensions
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IPhotoService, PhotoService>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.Configure<ClaudinarySettings>(config.GetSection("ClaudinarySettings"));
 
