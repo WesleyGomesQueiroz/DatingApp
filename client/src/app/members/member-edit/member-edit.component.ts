bd848e1cb08d29e72c51d4e1e8d72b084a5027c2
Wesley Queiroz <wesleygomesqueiroz@gmail.com>
2025-08-05 19:08:47 -0300

    Adding a photo uploader

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -46,7 +46,10 @@ export class MemberEditComponent implements OnInit {
         this.editForm?.reset(this.member);
       }
     })
+  }
 
+  onMemberChange(event: Member) {
+    this.member = event;
   }
 
 }
